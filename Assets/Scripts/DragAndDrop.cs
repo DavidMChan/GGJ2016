caecf9e91b3c697b5a9101f11e63005ec87089bf
Arman Darini <arman@officialhuman.com>
2016-01-30 03:58:12 -0700

    setup camera and builds for size 1024x768; added main background; added candle slots; added non-scrolling (yet) bottom UI; added dragable element

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler {
+
+  private Vector2 pointerOffset;
+//  private RectTransform canvasRectTransform;
+  private RectTransform panelRectTransform;
+  private RectTransform containerRectTransform;
+
+  void Awake () {
+    Canvas canvas = GetComponentInParent <Canvas>();
+    if (canvas != null) {
+//      canvasRectTransform = canvas.transform as RectTransform;
+      panelRectTransform = transform as RectTransform;
+      containerRectTransform = transform.parent as RectTransform;
+    }
+  }
+
+  public void OnPointerDown (PointerEventData data) {
+//    panelRectTransform.SetAsLastSibling ();
+    RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+  }
+
+  public void OnDrag (PointerEventData data) {
+    if (panelRectTransform == null)
+      return;
+
+    Vector2 pointerPostion = data.position;
+
+    Vector2 localPointerPosition;
+    if (RectTransformUtility.ScreenPointToLocalPointInRectangle (
+      containerRectTransform, pointerPostion, data.pressEventCamera, out localPointerPosition
+    )) {
+      panelRectTransform.localPosition = localPointerPosition - pointerOffset;
+    }
+  }
+}
