a3bcc7ae7b42ceaf593a6f645249484fd647fc36
Arman Darini <arman@officialhuman.com>
2016-01-30 15:21:34 -0700

    added all recipes; added temp animal prefabs

--- Assets/Scripts/AnimalSpawnController.cs
+++ Assets/Scripts/AnimalSpawnController.cs
@@ -3,35 +3,43 @@ using System.Collections;
 
 public class AnimalSpawnController : MonoBehaviour {
 
-    public Transform SpawnLocation;
-    public Transform WalkLoction;
-    public SacrificeManager sacman;
+  public Transform SpawnLocation;
+  public Transform WalkLoction;
+  public SacrificeManager sacman;
 
-    public GameObject COW;
+  public GameObject rabbit;
+  public GameObject chick;
+  public GameObject deer;
+  public GameObject panda;
+  public GameObject orangutang;
+  public GameObject demon;
 
-    public enum Animal
-    {
-        COW
+  public enum Animal {
+    Rabbit,
+    Chick,
+    Deer,
+    Panda,
+    Orangutang,
+    Demon
+  }
+
+  public GameObject SpawnAnimal(Animal an) {
+    if (an == Animal.Rabbit)
+      return (GameObject)Instantiate(rabbit, SpawnLocation.position, Quaternion.identity);
+    return null;
+  }
+
+  public void Update() {
+    if (GameStateManager.GetInstance().GetCurrentState() == GameStateManager.GameState.ANIMAL_SPAWNING) {
+      string nextKill = sacman.GetNextAnimal();
+      GameObject spawnedAnimal = null;
+      if (nextKill.Equals("Rabbit"))
+        spawnedAnimal = SpawnAnimal(Animal.Rabbit);
+      if (nextKill.Equals("Chick"))
+        spawnedAnimal = SpawnAnimal(Animal.Chick);
+
+      spawnedAnimal.GetComponent<AnimalMovementController>().sacrificeTargetLocation = WalkLoction;
+      GameStateManager.GetInstance().RequestGameStateChange(GameStateManager.GameState.ANIMAL_ENTERING);
     }
-
-    public GameObject SpawnAnimal(Animal an)
-    {
-        if (an == Animal.COW)
-            return (GameObject) Instantiate(COW, SpawnLocation.position, Quaternion.identity);
-        return null;
-    }
-
-    public void Update()
-    {
-        if (GameStateManager.GetInstance().GetCurrentState() == GameStateManager.GameState.ANIMAL_SPAWNING)
-        {
-            string nextKill = sacman.GetNextAnimal();
-            GameObject spawnedAnimal = null;
-            if (nextKill.Equals("Rabbit"))
-                spawnedAnimal = SpawnAnimal(Animal.COW);
-
-            spawnedAnimal.GetComponent<AnimalMovementController>().sacrificeTargetLocation = WalkLoction;
-            GameStateManager.GetInstance().RequestGameStateChange(GameStateManager.GameState.ANIMAL_ENTERING);
-        }
-    }
+  }
 }
