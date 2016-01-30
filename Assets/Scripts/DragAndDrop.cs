4442025eb4a7844b7111cb2b1cb2241fb82ac167
David Chan <David Chan>
2016-01-30 08:40:31 -0700

    Added snapping support

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -3,12 +3,19 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler {
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler {
 
   private Vector2 pointerOffset;
 //  private RectTransform canvasRectTransform;
   private RectTransform panelRectTransform;
   private RectTransform containerRectTransform;
+
+  public SnapPoint homeLocation;
+  public float snapRadius = 30;
+    public float snapSpeed = 10;
+
+  private SnapPoint destination = null;
+  private bool moving = false;
 
   void Awake () {
     Canvas canvas = GetComponentInParent <Canvas>();
@@ -37,4 +44,33 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IDragHandler {
       panelRectTransform.localPosition = localPointerPosition - pointerOffset;
     }
   }
+
+  public void OnEndDrag (PointerEventData data)
+  {
+        SnapPoint where = GameObject.FindObjectOfType<SnappingPointRegistry>().returnClosestSnapPoint(this.gameObject.transform);
+        if (where == null || Vector2.Distance(this.gameObject.transform.position, where.gameObject.transform.position) > snapRadius)
+        {
+            destination = homeLocation;
+            moving = true;
+        }
+        else
+        {
+            destination = where;
+            moving = true;
+        }
+
+  }
+
+  public void Update() {
+        if (this.moving)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, this.destination.gameObject.transform.position, snapSpeed * Time.deltaTime);
+            if (Vector2.Distance(this.transform.position, this.destination.gameObject.transform.position) < .01)
+            {
+                this.destination = null;
+                this.moving = false;
+            }
+        }
+}
+
 }
---
+++ Assets/SnappingPointRegistry.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnappingPointRegistry : MonoBehaviour {
+
+    //Array of possible snap points for drag and drop
+    public SnapPoint[] snaps;
+
+    //Returns the snap point which is closest to the current object
+    public SnapPoint returnClosestSnapPoint(Transform snapLocation)
+    {
+        SnapPoint closest = null;
+        float distance = 0;
+        foreach (SnapPoint point in snaps)
+        {
+            if (closest == null)
+            {
+                closest = point;
+                distance = Vector2.Distance(point.gameObject.transform.position, snapLocation.gameObject.transform.position);
+            }
+            else if (Vector2.Distance(point.gameObject.transform.position, snapLocation.gameObject.transform.position) < distance)
+            {
+                closest = point;
+                distance = Vector2.Distance(point.gameObject.transform.position, snapLocation.gameObject.transform.position);
+            }
+        }
+        return closest;
+    }
+}
