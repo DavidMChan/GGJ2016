da9b35758bac667d9230070054d76ee7779a98dd
David Chan <davidchan@cs.du.edu>
2016-01-30 12:23:37 -0700

    Added some ghetto looking menus

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public void SwitchScene(string Scene)
+    {
+        Application.LoadLevel(Scene);
+    }
+}
