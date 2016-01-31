e18bbef7c9e24aa23999bb0a1a5979406f8faca0
David Chan <davidchan@cs.du.edu>
2016-01-31 15:46:35 -0700

    Added flicker

---
+++ Assets/Flicker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+    public float maxint = 0;
+    public float minint = 0;
+    public float multiplier = 1;
+
+
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Light>().intensity = (minint + maxint) / 2 + Mathf.Sin(multiplier*Time.realtimeSinceStartup); 
+	}
+}
